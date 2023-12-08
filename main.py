51ae3eb13c5932c236fad1f97dc8b108d96387be
ad3rinto <aad3rinto@gmail.com>
2023-12-08 11:44:52 +0000

    setting up the UI

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import json
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("FLASHY")
+window.config(background=BACKGROUND_COLOR)
+window.mainloop()
