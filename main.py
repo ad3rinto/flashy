14e0e1fa23d42542b94027d82d2023b4baba8141
ad3rinto <aad3rinto@gmail.com>
2023-12-13 11:01:49 +0000

    addded canvas

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("FLASHY")
-window.config(background=BACKGROUND_COLOR)
+window.config(background=BACKGROUND_COLOR, padx=50, pady=50)
 
-right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
-right_button.pack()
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, rowspan=2)
 
-left_image = PhotoImage(file="./images/wrong.png")
-left_button = Button(image=left_image, highlightthickness=0)
-left_button.pack()
+
+# right_image = PhotoImage(file="./images/right.png")
+# right_button = Button(image=right_image, highlightthickness=0)
+# right_button.grid(row=2, column=0)
+#
+# left_image = PhotoImage(file="./images/wrong.png")
+# left_button = Button(image=left_image, highlightthickness=0)
+# left_button.grid(row=2, column=1)
 
 window.mainloop()
