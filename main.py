76d16f5592cb3e9c6694abda377f49cf005253d0
ad3rinto <aad3rinto@gmail.com>
2023-12-13 09:51:54 +0000

    create buttons

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("FLASHY")
 window.config(background=BACKGROUND_COLOR)
+
+right_image = PhotoImage(file="./images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.pack()
+
+left_image = PhotoImage(file="./images/wrong.png")
+left_button = Button(image=left_image, highlightthickness=0)
+left_button.pack()
+
 window.mainloop()
